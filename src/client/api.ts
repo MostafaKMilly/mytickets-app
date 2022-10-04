50ebecf5f43b5dc29c996a331ec20f4440de67fa
Mostafa Milly <m.milly@workwithnerds.ca>
2022-10-04 19:19:53 +0300

    Finish basics of https client

---
+++ src/client/api.ts
@@ -0,0 +1,68 @@
+import axios from "axios"
+import { TDeleteRquest, TGetRequest, TPostRequst, TPutRequest } from "../types";
+
+const instance = axios.create({
+    baseURL: "https://mytickets-cms.herokuapp.com/api/",
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+    }
+})
+
+// Request interceptor to pass token from local storage in request headers
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+    const auth = token ? `Bearer ${token}` : '';
+    config.headers = {
+        ...config.headers,
+        authorization: auth
+    }
+})
+
+
+/**
+ * 
+ * @param {TGetRequest} requestConfig - get request config
+ * @returns Promise
+ */
+const get = <T, R>({ url, ...config }: TGetRequest<T, R>): Promise<T> => {
+    return instance.get(url, config);
+}
+
+/**
+ * 
+ * @param {TPostRequst} requestConfig - post request config
+ * @returns Promise
+ */
+const post = <T, R>({ url, data, ...config }: TPostRequst<T, R>): Promise<T> => {
+    return instance.post(url, data, config);
+}
+
+/**
+ * 
+ * @param {TPutRequest} requestConfig - put request config
+ * @returns Promise
+ */
+const put = <T, R>({ url, data, ...config }: TPutRequest<T, R>): Promise<T> => {
+    return instance.put(url, data, config)
+}
+
+/**
+ * 
+ * @param {TDeleteRquest} requestConfig - delete request config
+ * @returns Promise
+ */
+const remove = <T, R>({ url, params }: TDeleteRquest): Promise<T> => {
+    return instance.delete(url, { params })
+}
+
+
+const API = {
+    get,
+    post,
+    put,
+    remove
+}
+
+export default API
--- src/types/request.ts
+++ src/types/request.ts
@@ -1,6 +1,23 @@
+import { AxiosResponse } from "axios";
+
 export type MapResponseFn<T, R = unknown> = (res: R) => T;
 
-export type TGetRequest = {
-    url: string;
+export type TGetRequest<T, R> = {
+    url: string,
+    params?: Record<string, any>,
+    transformResponse?: (data: AxiosResponse<R>) => T
+}
+
+export type TPostRequst<T, R> = {
+    url: string,
+    data: Record<string, any>,
+    params?: Record<string, any>,
+    transformResponse?: (data: AxiosResponse<R>) => T
+}
+
+export type TPutRequest<T, R> = TPostRequst<T, R>
+
+export type TDeleteRquest = {
+    url: string,
     params?: Record<string, any>
 }
