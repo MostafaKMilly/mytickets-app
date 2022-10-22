fbb22d852e56e58f8f505319751fa5ecfc7aa15e
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2022-10-22 19:00:44 +0300

    test the created theme

--- src/pages/homePage/Home.tsx
+++ src/pages/homePage/Home.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
-  console.log(process.env.REACT_APP_API_URI)
-  const navigte = useNavigate()
-  const id = Math.random()*100
-  return (
-    <>
-      <div>
-        MY TICKETS APP HOMEPAGE
-      </div>
-      <button onClick={() =>navigte(`project/${id.toFixed(0)}`)}> create new project </button>
-    </>
-  );
+	console.log(process.env.REACT_APP_API_URI)
+	const navigte = useNavigate()
+	const id = Math.random() * 100
+	return (
+		<>
+			<div>
+				<Typography color={'primary'}>
+					MY TICKETS APP HOMEPAGE
+				</Typography>
+				<Button color={'secondary'} onClick={() => navigte(`project/${id.toFixed(0)}`)}> create new project </Button>
+			</div>
+		</>
+	);
 }
 
 export default Home;
