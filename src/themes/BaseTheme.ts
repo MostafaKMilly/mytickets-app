3ab050847c8f0e700ded961921bee4e989e167fc
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2022-10-22 18:51:54 +0300

    add style for Mui-Button component  + change the colors

--- src/themes/BaseTheme.ts
+++ src/themes/BaseTheme.ts
@@ -1,15 +1,33 @@
 import { createTheme } from '@mui/material/styles';
 
 const primaryColor = {
-	light: "#ecd6f0",
-	main: "#9c27b0",
-	dark: "#4b0150",
+	light: "#C34AED",
+	main: "#AB31D6",
+	dark: "#930FBF",
 };
 
 const secondaryColor = {
-	main: "#ffe2a7",
-	light: "#fcf8e8",
-	dark: "#dcbc8c",
+	light: "#FFFFD3",
+	main: "#FFE2A7",
+	dark: "#D1B67D",
+};
+
+const successColor = {
+	light: "#78FF9A",
+	main: "#48E674",
+	dark: "#00BD4F",
+};
+
+const errorColor = {
+	light: "#FF645F",
+	main: "#E94848",
+	dark: "#CB2932",
+};
+
+const warningColor = {
+	light: "#FFE65C",
+	main: "#F5BF33",
+	dark: "#CA9900",
 };
 
 const fontColor = "#302d2d";
@@ -22,11 +40,36 @@ export const theme = createTheme({
 		secondary: {
 			...secondaryColor,
 		},
+		success: {
+			...successColor,
+		},
+		error: {
+			...errorColor,
+		},
+		warning: {
+			...warningColor,
+		},
 	},
 	typography: {
 		allVariants: {
-			fontFamily: "Segoe UI",
+			fontFamily: 'Inter',
+			fontStyle: 'normal',
 			color: fontColor,
 		},
 	},
+	components: {
+		MuiButton: {
+			defaultProps: {
+				disableElevation: true,
+				variant: "contained",
+			},
+			styleOverrides: {
+				root: {
+					borderRadius: "3.125rem",
+					fontWeight: 600,
+					padding: "0.625rem 3.25rem 0.625rem 3.25rem",
+				},
+			},
+		},
+	}
 });
